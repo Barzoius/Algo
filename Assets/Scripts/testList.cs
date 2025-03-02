1025b3fcb1af2002307b7a4b9e4aff9f22cc21ea
Barzoius <mecca.rares@gmail.com>
2025-03-02 02:38:36 +0200

    DUMB SCROLL LIST

--- Assets/Scripts/testList.cs
+++ Assets/Scripts/testList.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class testList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject textPrefab; // Assign a TextMeshProUGUI prefab
+    public Transform contentPanel; // Assign the "Content" object of the Scroll View
 
-    // Update is called once per frame
+    private int counter = 1;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) // Detects left-click
+        {
+            AddNewText("New Entry " + counter);
+            counter++;
+        }
+    }
+
+    void AddNewText(string text)
+    {
+        GameObject newText = Instantiate(textPrefab, contentPanel);
+        newText.GetComponent<TMP_Text>().text = text;
     }
 }
